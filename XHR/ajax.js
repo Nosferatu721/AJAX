26d7b578878f1686d923d52110286a7ff06b7e1e
Nosferatu721 <elkintorres721@gmail.com>
2020-12-20 12:24:06 -0500

    Usando XMLHttpReequest

--- XHR/ajax.js
+++ XHR/ajax.js
@@ -7,17 +7,23 @@
     if (xhr.readyState !== 4) return;
 
     if (xhr.status >= 200 && xhr.status < 300) {
-      console.log("Exito");
+      //console.log("Exito");
       //console.log(xhr.responseText)
       let json = JSON.parse(xhr.responseText);
       console.log(json);
+
       json.forEach((it) => {
         const $li = document.createElement("li");
-        $li.innerHTML = `${it.name}`;
-        $xhr.insertAdjacentElement("afterbegin", $li);
+        $li.innerHTML = `${it.name} -- ${it.email}`;
+        // Se usa el Fragmento Para realizar solo una insercion
+        $fragment.appendChild($li);
       });
+
+      $xhr.appendChild($fragment);
     } else {
-      console.log("Exito");
+      console.log(xhr);
+      let messageError = xhr.statusText || "Ocurrio un Error";
+      $xhr.innerHTML = `Error ${messageError}.`;
     }
   });
 
